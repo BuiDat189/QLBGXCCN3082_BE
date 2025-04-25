ea73989f9e4d848581ee7ea78db956da70921bb0
BuiDat189 <lebaan1203@gmail.com>
2025-04-26 00:26:50 +0700

    BE6

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CuDanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,4 +24,13 @@ Route::group(['prefix'  =>  '/admin'], function () {
     Route::post('/thong-tin-tim', [AdminController::class, 'timAdmin']);
     Route::post('/doi-mat-khau', [AdminController::class, 'doiPass']);
     Route::get('/lay-du-lieu-profile', [AdminController::class, 'getDataProfile']);
+
+    Route::group(['prefix'  =>  '/cu-dan'], function () {
+        Route::get('/lay-du-lieu', [CuDanController::class, 'getData']);
+        Route::post('/them-du-lieu', [CuDanController::class, 'themCuDan']);
+        Route::post('/thong-tin-cap-nhat', [CuDanController::class, 'capnhatCuDan']);
+        Route::post('/doi-trang-thai', [CuDanController::class, 'doiTrangThaiCuDan']);
+        Route::delete('/thong-tin-xoa/{id}', [CuDanController::class, 'xoaCuDan']);
+        Route::post('/doi-mat-khau', [CuDanController::class, 'doiPass']);
+    });
 });
