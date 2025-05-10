223fd6aa6585972c3993fde30f6d9b4fd959a310
Khoa-CNTT <vietdat18092003@gmail.com>
2025-05-10 11:42:25 +0700

    fix

--- app/Http/Controllers/CuDanController.php
+++ app/Http/Controllers/CuDanController.php
@@ -7,12 +7,49 @@ use App\Http\Requests\DoiPassAdminReuqest;
 use App\Http\Requests\ThemCuDanRequest;
 use App\Models\CuDan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CuDanController extends Controller
 {
     public function getData()
     {
-        $cudan = CuDan::all();
+        $cudan = CuDan::join('can_hos', 'can_hos.id', '=', 'cu_dans.id_can_ho')
+            ->leftJoin('xes', function($join) {
+                $join->on('xes.id_cu_dan', '=', 'cu_dans.id');
+            })
+            ->select(
+                'cu_dans.id',
+                'cu_dans.ho_va_ten',
+                'cu_dans.email',
+                'cu_dans.password',
+                'cu_dans.so_dien_thoai',
+                'cu_dans.so_cccd',
+                'cu_dans.id_can_ho',
+                'cu_dans.so_du',
+                'cu_dans.phe_duyet',
+                'cu_dans.trang_thai',
+                'can_hos.ten_toa_nha',
+                'can_hos.tang',
+                'can_hos.so_can_ho',
+                DB::raw('MIN(xes.bien_so_xe) as bien_so_xe')
+            )
+            ->groupBy(
+                'cu_dans.id',
+                'cu_dans.ho_va_ten',
+                'cu_dans.email',
+                'cu_dans.password',
+                'cu_dans.so_dien_thoai',
+                'cu_dans.so_cccd',
+                'cu_dans.id_can_ho',
+                'cu_dans.so_du',
+                'cu_dans.phe_duyet',
+                'cu_dans.trang_thai',
+                'can_hos.ten_toa_nha',
+                'can_hos.tang',
+                'can_hos.so_can_ho'
+            )
+            ->get();
         return response()->json([
             'status' => true,
             'message' => 'Lấy dữ liệu thành công',
@@ -24,11 +61,10 @@ class CuDanController extends Controller
         CuDan::create([
             'ho_va_ten' => $request->ho_va_ten,
             'email' => $request->email,
-            'password' => bcrypt($request->password),
+            'password' => bcrypt(123456),
             'so_dien_thoai' => $request->so_dien_thoai,
             'so_cccd' => $request->so_cccd,
             'id_can_ho' => $request->id_can_ho,
-            'so_du' => $request->so_du,
         ]);
         return response()->json([
             'status'   => true,
@@ -45,7 +81,6 @@ class CuDanController extends Controller
                 'so_dien_thoai' => $request->so_dien_thoai,
                 'so_cccd' => $request->so_cccd,
                 'id_can_ho' => $request->id_can_ho,
-                'so_du' => $request->so_du,
             ]);
             return response()->json([
                 'status'   => true,
@@ -105,6 +140,112 @@ class CuDanController extends Controller
         return response()->json([
             'status'  =>  true,
             'message' =>  'Đổi mật khẩu thành công'
+        ]);
+    }
+    public function login(Request $request)
+    {
+        $check = Auth::guard('cu_dan')->attempt(['email' => $request->email, 'password' => $request->password,]);
+        if ($check) {
+            $user = Auth::guard('cu_dan')->user();
+            if ($user->trang_thai == 0) {
+                return response()->json([
+                    'status' => false,
+                    'message' => "Tài khoản của bạn đã bị khóa!"
+                ]);
+            } elseif ($user->phe_duyet == 0) {
+                return response()->json([
+                    'status' => false,
+                    'message' => "Tài khoản của bạn đang chờ duyệt!"
+                ]);
+            } else {
+                return response()->json([
+                    'status' => true,
+                    'message' => "Đăng nhập thành công!",
+                    'token' => $user->createToken('cu_dan_token')->plainTextToken,
+                    'user' => $user
+                ]);
+            }
+        } else {
+            return response()->json([
+                'message'   => 'Thông tin đăng nhập không chính xác!!',
+                'status'    => false
+            ]);
+        }
+    }
+    public function register(ThemCuDanRequest $request)
+    {
+        // if ($request->password != $request->password_confirm) {
+        //     return response()->json([
+        //         'message'   => 'Mật khẩu không khớp!!',
+        //         'status'    => false
+        //     ]);
+        // }
+        CuDan::create([
+            'ho_va_ten' => $request->ho_va_ten,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'so_dien_thoai' => $request->so_dien_thoai,
+            'so_cccd' => $request->so_cccd,
+            'id_can_ho' => $request->id_can_ho,
+        ]);
+        return response()->json([
+            'message'   => 'Tạo tài khoản thành công!!',
+            'status'    =>  true
+        ]);
+    }
+    public function checkToken()
+    {
+
+        $user = $this->isCuDan();
+
+        if ($user) {
+            return response()->json([
+                'message'   => 'Token còn hiệu lực',
+                'status'    => true,
+                'user'      => $user,
+            ]);
+        }
+        return response()->json([
+            'message'   => 'Token không còn hiệu lực',
+            'status'    => false
+        ]);
+    }
+    public function logout()
+    {
+        $user = $this->isCuDan();
+        if ($user) {
+            DB::table('personal_access_tokens')
+                ->where('id', $user->currentAccessToken()->id)->delete();
+
+            return response()->json([
+                'message'   => 'Đăng xuất thành công!!',
+                'status'    => true
+            ]);
+        }
+        return response()->json([
+            'message'   => 'Bạn chưa đăng nhập tài khoản cư dân!',
+            'status'    => false
+        ]);
+    }
+    public function duyetCuDan(Request $request)
+    {
+
+
+        $cudan = CuDan::find($request->id);
+        if (!$cudan) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Cư dân không tồn tại'
+            ]);
+        }
+
+        $cudan->update([
+            'phe_duyet' => 1
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Cư dân đã được duyệt'
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AdminThongBaoController;
 use App\Http\Controllers\BaiXeController;
+use App\Http\Controllers\BaoCaoKhachVangLaiController;
+use App\Http\Controllers\BaoCaoSuCoController;
+use App\Http\Controllers\CamGiamSoatController;
 use App\Http\Controllers\CanHoController;
 use App\Http\Controllers\ChiTietChucVuController;
 use App\Http\Controllers\ChucNangController;
@@ -13,6 +16,7 @@ use App\Http\Controllers\LichSuRaVaoBaiXeController;
 use App\Http\Controllers\LoaiXeController;
 use App\Http\Controllers\ViTriDatController;
 use App\Http\Controllers\XeController;
+use Database\Seeders\BaoCaoKhachVangLaiSeeder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,11 +24,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-    Route::post('/admin/login', [AdminController::class, 'login']);
-    Route::post('/admin/register', [AdminController::class, 'register']);
-    Route::get('/admin/check-token', [AdminController::class, 'checkToken']);
-    Route::post('/admin/logout', [AdminController::class, 'logout']);
 
+///      ADMIN
+Route::post('/admin/login', [AdminController::class, 'login']);
+Route::post('/admin/register', [AdminController::class, 'register']);
+Route::get('/admin/check-token', [AdminController::class, 'checkToken']);
+Route::post('/admin/logout', [AdminController::class, 'logout']);
 
 Route::group(['prefix'  =>  '/admin'], function () {
     Route::get('/lay-du-lieu', [AdminController::class, 'getData']);
@@ -43,7 +48,6 @@ Route::group(['prefix'  =>  '/admin'], function () {
         Route::post('/doi-trang-thai', [ChucVuController::class, 'doiTrangThaiChucVu']);
         Route::delete('/thong-tin-xoa/{id}', [ChucVuController::class, 'xoaChucVu']);
         Route::post('/thong-tin-tim', [ChucVuController::class, 'timChucVu']);
-
     });
 
     Route::group(['prefix'  =>  '/phan-quyen'], function () {
@@ -53,7 +57,6 @@ Route::group(['prefix'  =>  '/admin'], function () {
         Route::get('/chuc-nang/data', [ChucNangController::class, 'getData']);
     });
 
-
     Route::group(['prefix'  =>  '/cu-dan'], function () {
         Route::get('/lay-du-lieu', [CuDanController::class, 'getData']);
         Route::post('/them-du-lieu', [CuDanController::class, 'themCuDan']);
@@ -61,44 +64,24 @@ Route::group(['prefix'  =>  '/admin'], function () {
         Route::post('/doi-trang-thai', [CuDanController::class, 'doiTrangThaiCuDan']);
         Route::delete('/thong-tin-xoa/{id}', [CuDanController::class, 'xoaCuDan']);
         Route::post('/doi-mat-khau', [CuDanController::class, 'doiPass']);
+        Route::post('/duyet', [CuDanController::class, 'duyetCuDan']);
+
     });
+
     Route::group(['prefix'  =>  '/can-ho'], function () {
         Route::get('/lay-du-lieu', [CanHoController::class, 'getData']);
         Route::post('/them-du-lieu', [CanHoController::class, 'themCanHo']);
         Route::post('/thong-tin-cap-nhat', [CanHoController::class, 'capnhatCanHo']);
         Route::delete('/thong-tin-xoa/{id}', [CanHoController::class, 'xoaCanHo']);
     });
-    Route::group(['prefix'  =>  '/Xe'], function () {
+
+    Route::group(['prefix'  =>  '/xe'], function () {
         Route::get('/lay-du-lieu', [XeController::class, 'getData']);
         Route::post('/them-du-lieu', [XeController::class, 'themXe']);
         Route::post('/thong-tin-cap-nhat', [XeController::class, 'capnhatXe']);
         Route::post('/doi-trang-thai', [XeController::class, 'doiTrangThaiXe']);
         Route::delete('/thong-tin-xoa/{id}', [XeController::class, 'xoaXe']);
-    });
-
-    Route::group(['prefix'  =>  '/vi-tri-dat'], function () {
-        Route::get('/lay-du-lieu', [ViTriDatController::class, 'getData']);
-        Route::post('/them-du-lieu', [ViTriDatController::class, 'themViTriDat']);
-        Route::post('/thong-tin-cap-nhat', [ViTriDatController::class, 'capnhatViTriDat']);
-        Route::delete('/thong-tin-xoa/{id}', [ViTriDatController::class, 'xoaViTriDat']);
-    });
-    Route::group(['prefix'  =>  '/lich-su-ra-vao'], function () {
-        Route::get('/lay-du-lieu', [LichSuRaVaoBaiXeController::class, 'getLichSuRaVao']);
-    });
-
-    Route::group(['prefix'  =>  '/giao-dich'], function () {
-        Route::get('/lay-du-lieu', [GiaoDichController::class, 'getData']);
-    });
-
-    Route::group(['prefix'  =>  '/bai-xe'], function () {
-        Route::get('/lay-du-lieu', [BaiXeController::class, 'getData']);
-        Route::post('/them-du-lieu', [BaiXeController::class, 'themBaiXe']);
-        Route::post('/thong-tin-cap-nhat', [BaiXeController::class, 'capnhatBaiXe']);
-        Route::delete('/thong-tin-xoa/{id}', [BaiXeController::class, 'xoaBaiXe']);
-    });
-
-    Route::group(['prefix'  =>  '/thong-bao'], function () {
-        Route::get('/lay-du-lieu', [AdminThongBaoController::class, 'getData']);
+        Route::post('/duyet', [XeController::class, 'duyetXe']);
     });
 
     Route::group(['prefix'  =>  '/loai-xe'], function () {
@@ -109,4 +92,85 @@ Route::group(['prefix'  =>  '/admin'], function () {
         Route::delete('/thong-tin-xoa/{id}', [LoaiXeController::class, 'xoaLoaiXe']);
     });
 
+
+    Route::group(['prefix'  =>  '/lich-su-ra-vao'], function () {
+        Route::get('/lay-du-lieu', [LichSuRaVaoBaiXeController::class, 'getLichSuRaVao']);
+    });
+
+    Route::group(['prefix'  =>  '/thong-bao'], function () {
+        Route::get('/lay-du-lieu', [AdminThongBaoController::class, 'getData']);
+    });
+
+    Route::group(['prefix'  =>  '/giao-dich'], function () {
+        Route::get('/lay-du-lieu', [GiaoDichController::class, 'getData']);
+        Route::post('/them-du-lieu', [GiaoDichController::class, 'themGiaoDich']);
+        Route::post('/thong-tin-cap-nhat', [GiaoDichController::class, 'capnhatGiaoDich']);
+        Route::delete('/thong-tin-xoa/{id}', [GiaoDichController::class, 'xoaGiaoDich']);
+    });
+
+    Route::group(['prefix'  =>  '/bai-xe'], function () {
+        Route::get('/lay-du-lieu', [BaiXeController::class, 'getData']);
+        Route::post('/them-du-lieu', [BaiXeController::class, 'themBaiXe']);
+        Route::post('/thong-tin-cap-nhat', [BaiXeController::class, 'capnhatBaiXe']);
+        Route::delete('/thong-tin-xoa/{id}', [BaiXeController::class, 'xoaBaiXe']);
+    });
+
+    Route::group(['prefix'  =>  '/vi-tri-dat'], function () {
+        Route::get('/lay-du-lieu', [ViTriDatController::class, 'getData']);
+        Route::post('/them-du-lieu', [ViTriDatController::class, 'themViTriDat']);
+        Route::post('/thong-tin-cap-nhat', [ViTriDatController::class, 'capnhatViTriDat']);
+        Route::delete('/thong-tin-xoa/{id}', [ViTriDatController::class, 'xoaViTriDat']);
+    });
+    Route::group(['prefix'  =>  '/cam-giam-soat'], function () {
+        Route::get('/lay-du-lieu', [CamGiamSoatController::class, 'getData']);
+        Route::post('/them-du-lieu', [CamGiamSoatController::class, 'themCamGiamSoat']);
+        Route::post('/thong-tin-cap-nhat', [CamGiamSoatController::class, 'capnhatCamGiamSoat']);
+        Route::delete('/thong-tin-xoa/{id}', [CamGiamSoatController::class, 'xoaCamGiamSoat']);
+    });
+
+    Route::group(['prefix'  =>  '/bao-cao-khach-vang-lai'], function () {
+        Route::get('/lay-du-lieu', [BaoCaoKhachVangLaiController::class, 'getData']);
+        Route::post('/them-du-lieu', [BaoCaoKhachVangLaiController::class, 'themBaoCaoKhachVangLai']);
+        Route::post('/thong-tin-cap-nhat', [BaoCaoKhachVangLaiController::class, 'capnhatBaoCaoKhachVangLai']);
+        Route::delete('/thong-tin-xoa/{id}', [BaoCaoKhachVangLaiController::class, 'xoaBaoCaoKhachVangLai']);
+        Route::post('/thanh-toan', [BaoCaoKhachVangLaiController::class, 'thanhToanBaoCaoKhachVangLai']);
+
+    });
+
+    Route::group(['prefix'  =>  '/bao-cao-su-co'], function () {
+        Route::get('/lay-du-lieu', [BaoCaoSuCoController::class, 'getData']);
+        Route::post('/them-du-lieu', [BaoCaoSuCoController::class, 'themBaoCaoSuCo']);
+        Route::post('/thong-tin-cap-nhat', [BaoCaoSuCoController::class, 'capnhatBaoCaoSuCo']);
+        Route::delete('/thong-tin-xoa/{id}', [BaoCaoSuCoController::class, 'xoaBaoCaoSuCo']);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
+
+Route::post('/user/login', [CuDanController::class, 'login']);
+Route::post('/user/register', [CuDanController::class, 'register']);
+Route::get('/user/check-token', [CuDanController::class, 'checkToken']);
+Route::post('/user/logout', [CuDanController::class, 'logout']);
+
+Route::group(['prefix'  =>  '/user'], function () {
+
+});
+
