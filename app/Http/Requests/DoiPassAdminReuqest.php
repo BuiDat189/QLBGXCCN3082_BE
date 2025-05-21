d58cbd2de3dac42005d6a5b06a6ada996391c48f
Khoa-CNTT <vietdat18092003@gmail.com>
2025-05-21 14:18:12 +0700

    fix

--- app/Http/Requests/DoiPassAdminReuqest.php
+++ app/Http/Requests/DoiPassAdminReuqest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class DoiPassAdminReuqest extends FormRequest
+class DoiPassNhanSuReuqest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -14,18 +14,29 @@ class DoiPassAdminReuqest extends FormRequest
         return true;
     }
 
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
     public function rules(): array
     {
         return [
-            'password'          =>  'required|min:6|max:30',
-            're_password'       =>  'required|same:password',
+            'id'                    => 'required|exists:admins,id',
+            'password'              => 'required|min:6|max:30',
+            'password_confirmation' => 'required|same:password',
         ];
     }
     public function messages()
     {
         return [
-            'password.*'          =>  'Mật khẩu mới yêu cầu phải từ 6 đến 30 ký tự',
-            're_password.*'       =>  'Mật khẩu mới không trùng khớp',
+            'id.required'                     => 'ID là bắt buộc',
+            'id.exists'                       => 'ID không tồn tại',
+            'password.required'                 => 'Mật khẩu là bắt buộc',
+            'password.min'                      => 'Mật khẩu phải có ít nhất 6 ký tự',
+            'password.max'                      => 'Mật khẩu phải có tối đa 30 ký tự',
+            'password_confirmation.required'    => 'Mật khẩu xác nhận là bắt buộc',
+            'password_confirmation.same'        => 'Mật khẩu xác nhận không khớp',
         ];
     }
 }
